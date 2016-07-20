f2a87ada15fbfcb7ad4aaed4f62167ec638de0a2
Ben Behrman & Mason Brown & Walter Chabla <pair+bbehrman10.codingmason.walterchabla@devbootcamp.com>
2016-07-20 10:32:30 -0400

    Create validations for models

--- db/migrate/20160720093328_create_questions.rb
+++ db/migrate/20160720093328_create_questions.rb
@@ -1,4 +1,11 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.string :subject
+      t.string :body
+      t.integer :user_id, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20160720093333_create_answers.rb
+++ db/migrate/20160720093333_create_answers.rb
@@ -1,4 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+    create_table :answers do |t|
+      t.string :text
+      t.integer :question_id, foreign_key: true
+      t.integer :user_id, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
