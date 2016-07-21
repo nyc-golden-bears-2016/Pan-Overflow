7a434336232184f084eabb47cf66987079214fae
Mason Brown <email": null>
2016-07-21 16:38:30 -0400

    Add logic to add error messages if people submit empty answers or comments

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,5 +1,10 @@
 post '/answers' do
   redirect_nologin
+  if params[:answer]["body"].empty?
+    @errors = ["Honey Grahams, don't submit empty answers. That's not helpful"]
+    @question = Question.find(params[:answer]["question_id"].to_i)
+    erb :'questions/show'
+  else
   answer = Answer.new(params[:answer])
     if answer.save
       redirect "/questions/#{params[:answer]['question_id']}"
@@ -7,4 +12,5 @@ post '/answers' do
       @errors = answer.errors.full_messages
       erb :'answers/new'
     end
+  end
 end
