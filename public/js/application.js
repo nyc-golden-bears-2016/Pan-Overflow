506508ef8d5645d0aa227563c67f4fcd9314ad66
Mason Brown <mason.patrick.brown@gmail.com>
2016-07-22 10:03:57 -0400

    Solve merge conflicts

--- public/js/application.js
+++ public/js/application.js
@@ -12,13 +12,14 @@ $(document).ready(function() {
       data: $(event.target.children).each(function(index, el) {el.name; el.value})
     });
     request.done(function(response){
+      if ( $(event.target.children).val() == "false" ) {
+        alert("You have to be logged in to do that");
+      }
+      else {
         var currentVote = $(event.target).parent().parent().siblings().find("#vote-count").html();
         var newVote = parseInt(currentVote) + 1;
         $(event.target).parent().parent().siblings().find("#vote-count").html(newVote);
-      });
-    // it doesnt fail, if it fails it would return this
-    request.fail(function(jqXHR, textStatus){
-        alert("You can only vote once");
+        }
       });
     });
 
@@ -30,12 +31,14 @@ $(document).ready(function() {
       data: $(event.target.children).each(function(index, el) {el.name; el.value})
     });
     request.done(function(response){
+        if ( $(event.target.children).val() == "false" ) {
+        alert("You have to be logged in to do that");
+      }
+      else {
         var currentVote = $(event.target).parent().parent().siblings().find("#vote-count").html();
         var newVote = parseInt(currentVote) - 1;
         $(event.target).parent().parent().siblings().find("#vote-count").html(newVote);
-    });
-    request.fail(function(jqXHR, textStatus){
-       alert("You can only vote once")
+        }
       });
     });
 });
